fd9d3d38427e50cd96aba4bd0d6745de580b2114
Navaneeth J <Navaneethjawahar333@gmail.com>
2022-08-08 17:43:13 -0400

    unwanted url

--- detect_image/urls.py
+++ detect_image/urls.py
@@ -1,8 +1,8 @@
-from django.conf import settings
-from django.views.static import serve
-from django.urls import path
-urlpatterns = [
-    url(r'^detect_image/(?P<path>.*)$', serve, {
-        'document_root': settings.MEDIA_ROOT,
-    }),
-]
+# from django.conf import settings
+# from django.views.static import serve
+# from django.urls import path
+# urlpatterns = [
+#     url(r'^detect_image/(?P<path>.*)$', serve, {
+#         'document_root': settings.MEDIA_ROOT,
+#     }),
+# ]
